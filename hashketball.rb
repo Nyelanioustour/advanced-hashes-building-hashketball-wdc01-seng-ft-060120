b2776bdfae794ff0b3a0fa8586347d6cc2ce4bf7
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 22:21:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,9 @@
+require 'pry'
 def game_hash
   hash = {:home => {:team_name => "string", :colors => "string", :players => "string"}, :away => {:team_name => "string", :colors => "string", :players => "string"}
     
   }
-  
+  binding.pry 
   
 end
 
