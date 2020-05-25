6147c027884b18f884874f504a6e4f18a4038d7d
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 22:28:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,18 +1,20 @@
 require 'pry'
 def game_hash
-  hash = {:home => {:team_name => "string", :colors => "string", :players => [{:player_name => int,
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks
-    
-    
-    
-  },]}, :away => {:team_name => "string", :colors => "string", :players => [{},]}
+  hash = {:home => {:team_name => "string", 
+                    :colors => "string", 
+                    :players => [
+                      {:player_name => int,
+                      :number => int,
+                      :shoe => int,
+                      :points => int,:
+                      :rebounds => int,
+                      :assists => int,
+                      :steals => int,
+                      :blocks => int,
+                      :slam_dunks => int}
+                      ]
+      }, 
+          :away => {:team_name => "string", :colors => "string", :players => [{},]}
     
   }
   binding.pry 
