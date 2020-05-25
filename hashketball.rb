9efb719fbab38308c967946f574d472807e3a560
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 22:43:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,15 +43,15 @@ def game_hash
                       :blocks => 1,
                       :slam_dunks => 0},
                      
-                      {:player_name => 0,
-                      :number => 0,
-                      :shoe => 0,
-                      :points => 0,
-                      :rebounds => 0,
-                      :assists => 0,
-                      :steals => 0,
-                      :blocks => 0,
-                      :slam_dunks => 0}
+                      {:player_name => "Kemba Walker",
+                      :number => 33,
+                      :shoe => 15,
+                      :points => 6,
+                      :rebounds => 12,
+                      :assists => 12,
+                      :steals => 7,
+                      :blocks => 5,
+                      :slam_dunks => 12}
                       ]
       }, 
           :away => {:team_name => "string",
