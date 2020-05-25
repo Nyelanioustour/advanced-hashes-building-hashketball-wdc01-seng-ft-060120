633c970e8bf39df22e4d28cb645b078a939ebff6
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 22:36:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -12,6 +12,45 @@ def game_hash
                       :steals => 0,
                       :blocks => 0,
                       :slam_dunks => 0}
+                      
+                      {:player_name => 0,
+                      :number => 0,
+                      :shoe => 0,
+                      :points => 0,
+                      :rebounds => 0,
+                      :assists => 0,
+                      :steals => 0,
+                      :blocks => 0,
+                      :slam_dunks => 0}
+                      {:player_name => 0,
+                      :number => 0,
+                      :shoe => 0,
+                      :points => 0,
+                      :rebounds => 0,
+                      :assists => 0,
+                      :steals => 0,
+                      :blocks => 0,
+                      :slam_dunks => 0}
+                      
+                      {:player_name => 0,
+                      :number => 0,
+                      :shoe => 0,
+                      :points => 0,
+                      :rebounds => 0,
+                      :assists => 0,
+                      :steals => 0,
+                      :blocks => 0,
+                      :slam_dunks => 0}
+                     
+                      {:player_name => 0,
+                      :number => 0,
+                      :shoe => 0,
+                      :points => 0,
+                      :rebounds => 0,
+                      :assists => 0,
+                      :steals => 0,
+                      :blocks => 0,
+                      :slam_dunks => 0}
                       ]
       }, 
           :away => {:team_name => "string",
